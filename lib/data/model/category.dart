5295e2f764a7855636125d2781f4c59431200315
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-10-13 12:51:57 +0700

    Created Category Model

---
+++ lib/data/model/category.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class Category {
+  int id;
+  String name;
+  Category({
+    required this.id,
+    required this.name,
+  });
+
+  Category copyWith({
+    int? id,
+    String? name,
+  }) {
+    return Category(
+      id: id ?? this.id,
+      name: name ?? this.name,
+    );
+  }
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(
+      id: map['id'] as int,
+      name: map['name'] as String,
+    );
+  }
+
+  factory Category.fromJson(String source) =>
+      Category.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'Category(id: $id, name: $name)';
+
+  @override
+  bool operator ==(covariant Category other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id && other.name == name;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ name.hashCode;
+}
