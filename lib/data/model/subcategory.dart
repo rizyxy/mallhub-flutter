c3c566e24b54137960eb795dd6fbad72509ff624
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-10-13 13:40:35 +0700

    Changed SubCategory to SubCategoryModel

--- lib/data/model/subcategory.dart
+++ lib/data/model/subcategory.dart
@@ -1,45 +1,45 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
-class SubCategory {
+class SubCategoryModel {
   int id;
   int categoryId;
   String name;
-  SubCategory({
+  SubCategoryModel({
     required this.id,
     required this.categoryId,
     required this.name,
   });
 
-  SubCategory copyWith({
+  SubCategoryModel copyWith({
     int? id,
     int? categoryId,
     String? name,
   }) {
-    return SubCategory(
+    return SubCategoryModel(
       id: id ?? this.id,
       categoryId: categoryId ?? this.categoryId,
       name: name ?? this.name,
     );
   }
 
-  factory SubCategory.fromMap(Map<String, dynamic> map) {
-    return SubCategory(
+  factory SubCategoryModel.fromMap(Map<String, dynamic> map) {
+    return SubCategoryModel(
       id: map['id'] as int,
       categoryId: map['category_id'] as int,
       name: map['name'] as String,
     );
   }
 
-  factory SubCategory.fromJson(String source) =>
-      SubCategory.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory SubCategoryModel.fromJson(String source) =>
+      SubCategoryModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() =>
-      'SubCategory(id: $id, categoryId: $categoryId, name: $name)';
+      'SubCategoryModel(id: $id, categoryId: $categoryId, name: $name)';
 
   @override
-  bool operator ==(covariant SubCategory other) {
+  bool operator ==(covariant SubCategoryModel other) {
     if (identical(this, other)) return true;
 
     return other.id == id &&
