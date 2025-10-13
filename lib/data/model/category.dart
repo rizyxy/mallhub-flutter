c17084612da74f24f5cfde9844b76f652e7a0f0b
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-10-13 13:29:42 +0700

    Changed Category to CategoryModel

--- lib/data/model/category.dart
+++ lib/data/model/category.dart
@@ -1,39 +1,39 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
-class Category {
+class CategoryModel {
   int id;
   String name;
-  Category({
+  CategoryModel({
     required this.id,
     required this.name,
   });
 
-  Category copyWith({
+  CategoryModel copyWith({
     int? id,
     String? name,
   }) {
-    return Category(
+    return CategoryModel(
       id: id ?? this.id,
       name: name ?? this.name,
     );
   }
 
-  factory Category.fromMap(Map<String, dynamic> map) {
-    return Category(
+  factory CategoryModel.fromMap(Map<String, dynamic> map) {
+    return CategoryModel(
       id: map['id'] as int,
       name: map['name'] as String,
     );
   }
 
-  factory Category.fromJson(String source) =>
-      Category.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory CategoryModel.fromJson(String source) =>
+      CategoryModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() => 'Category(id: $id, name: $name)';
 
   @override
-  bool operator ==(covariant Category other) {
+  bool operator ==(covariant CategoryModel other) {
     if (identical(this, other)) return true;
 
     return other.id == id && other.name == name;
